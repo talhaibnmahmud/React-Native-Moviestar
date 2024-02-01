18195ad0fb63274d9803c3ece6858554d52eda67
Talha Ibne Mahmud <71333752+talhaibnmahmud@users.noreply.github.com>
2024-02-01 14:59:39 +0600

    🧼 Update: Movie details page

--- app/(drawer)/home/movie/[id].tsx
+++ app/(drawer)/home/movie/[id].tsx
@@ -1,9 +1,23 @@
-import { Text, View } from 'react-native'
+import { colorTokens } from '@tamagui/themes'
+import { View } from 'react-native'
+import { Title } from '~/tamagui.config'
 
 const Movie = () => {
     return (
-        <View>
-            <Text>Movie 1</Text>
+        <View style={{
+            flex: 1,
+            alignItems: "center",
+            justifyContent: "center",
+            gap: 20,
+            backgroundColor: colorTokens.light.green.green3,
+        }}
+        >
+            <Title style={{
+                color: colorTokens.light.green.green11,
+                fontSize: 30,
+                fontWeight: "bold",
+            }}
+            >Movie 1</Title>
         </View>
     )
 }
